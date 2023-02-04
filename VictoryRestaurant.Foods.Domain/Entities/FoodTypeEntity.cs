38d84c578020b747015aa61b5426f34342993e84
DimaInNature <dimainnature@yandex.ru>
2023-02-04 23:28:12 +0300

    feat: Add FoodTypeEntity.

---
+++ VictoryRestaurant.Foods.Domain/Entities/FoodTypeEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace VictoryRestaurant.Foods.Domain.Entities;
+
+/// <summary> Food type entity. </summary>
+public sealed class FoodTypeEntity
+{
+    /// <summary> Identifier. </summary>
+    public Guid Id { get; set; }
+
+    /// <summary> Type name. </summary>
+    public string Name { get; set; } = default!;
+}
