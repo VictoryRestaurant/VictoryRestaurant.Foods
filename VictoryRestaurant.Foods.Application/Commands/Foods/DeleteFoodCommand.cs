6917ab668b446350699c8e1f62d3c94d9e2cf2e4
DimaInNature <dimainnature@yandex.ru>
2023-02-05 14:09:43 +0300

    feat: Add DeleteFoodCommand.

---
+++ VictoryRestaurant.Foods.Application/Commands/Foods/DeleteFoodCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace VictoryRestaurant.Foods.Application.Commands.Foods;
+
+public sealed record class DeleteFoodCommand : IRequest
+{
+    public Guid Id { get; }
+
+    public DeleteFoodCommand(Guid id) => Id = id;
+
+    public DeleteFoodCommand() { }
+
+    public sealed record class Handler : IRequestHandler<DeleteFoodCommand>
+    {
+        private readonly IFoodEntityRepository _repository;
+
+        public Handler(IFoodEntityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteFoodCommand request,
+            CancellationToken cancellationToken)
+        {
+            if(request.Id == Guid.Empty)
+            {
+                return default;
+            }
+
+            await _repository.DeleteAsync(id: request.Id, cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+
+            return default;
+        }
+    }
+}
