260255cab4427a4b129029bf482fc1e7bc4108a5
DimaInNature <dimainnature@yandex.ru>
2023-02-07 23:48:51 +0300

    feat: Add GetAllFoodTypeListQuery.

---
+++ src/VictoryRestaurant.Foods.Application/Queries/FoodTypes/GetAllFoodTypeListQuery.cs
@@ -0,0 +1,23 @@
+﻿namespace VictoryRestaurant.Foods.Application.Queries.FoodTypes;
+
+public sealed record class GetAllFoodTypeListQuery : IRequest<IEnumerable<FoodTypeEntity>>
+{
+    public sealed record class Handler : IRequestHandler<GetAllFoodTypeListQuery, IEnumerable<FoodTypeEntity>>
+    {
+        private readonly IFoodTypeEntityRepository _repository;
+
+        public Handler(IFoodTypeEntityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<FoodTypeEntity>> Handle(
+            GetAllFoodTypeListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var foodTypes = await _repository.GetAllAsync(cancellationToken);
+
+            return foodTypes;
+        }
+    }
+}
