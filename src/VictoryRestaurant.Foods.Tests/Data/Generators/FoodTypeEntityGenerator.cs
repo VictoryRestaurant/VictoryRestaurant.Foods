e8024af5c9bbe1efa652ad33c5737b43cbc53ca2
DimaInNature <dimainnature@yandex.ru>
2023-02-08 01:52:47 +0300

    feat: Create FoodTypeEntityGenerator.

---
+++ src/VictoryRestaurant.Foods.Tests/Data/Generators/FoodTypeEntityGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace VictoryRestaurant.Foods.Tests.Data.Generators;
+
+public static class FoodTypeEntityGenerator
+{
+    public static FoodTypeEntity GenerateFoodTypeEntity()
+    {
+        var faker = new Faker();
+
+        FoodTypeEntity foodType = new()
+        {
+            Id = faker.Random.Guid(),
+            Name = faker.Commerce.FoodType()
+        };
+
+        return foodType;
+    }
+}
