15c7438a7b96b8d671b070efb46dbaf66f8658db
DimaInNature <dimainnature@yandex.ru>
2023-02-07 23:49:08 +0300

    feat: Add GetFoodTypeByConditionQuery.

---
+++ src/VictoryRestaurant.Foods.Application/Queries/FoodTypes/GetFoodTypeByConditionQuery.cs
@@ -0,0 +1,36 @@
+﻿namespace VictoryRestaurant.Foods.Application.Queries.FoodTypes;
+
+public sealed record class GetFoodTypeByConditionQuery : IRequest<FoodTypeEntity?>
+{
+    public Func<FoodTypeEntity, bool>? Predicate { get; }
+
+    public GetFoodTypeByConditionQuery(
+        Func<FoodTypeEntity, bool> predicate) =>
+        Predicate = predicate;
+
+    public GetFoodTypeByConditionQuery() { }
+
+    public sealed record class Handler : IRequestHandler<GetFoodTypeByConditionQuery, FoodTypeEntity?>
+    {
+        private readonly IFoodTypeEntityRepository _repository;
+
+        public Handler(IFoodTypeEntityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<FoodTypeEntity?> Handle(
+            GetFoodTypeByConditionQuery request,
+            CancellationToken cancellationToken)
+        {
+            if (request.Predicate is null)
+            {
+                return default;
+            }
+
+            var foodType = await _repository.GetAsync(predicate: request.Predicate, cancellationToken);
+
+            return foodType;
+        }
+    }
+}
