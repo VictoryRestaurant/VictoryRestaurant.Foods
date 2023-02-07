c087b2b13f32990d78986f1dadea126c28f8a670
DimaInNature <dimainnature@yandex.ru>
2023-02-07 23:19:52 +0300

    feat: Add UpdateFoodTypeCommand.

---
+++ src/VictoryRestaurant.Foods.Application/Commands/FoodTypes/UpdateFoodTypeCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace VictoryRestaurant.Foods.Application.Commands.FoodTypes;
+
+public sealed record class UpdateFoodTypeCommand : IRequest<FoodEntity?>
+{
+    public FoodEntity? Food { get; }
+
+    public UpdateFoodTypeCommand(FoodEntity food) => Food = food;
+
+    public UpdateFoodTypeCommand() { }
+
+    public sealed record class Handler : IRequestHandler<UpdateFoodTypeCommand, FoodEntity?>
+    {
+        private readonly IFoodEntityRepository _repository;
+
+        public Handler(IFoodEntityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<FoodEntity?> Handle(
+            UpdateFoodTypeCommand request,
+            CancellationToken cancellationToken)
+        {
+            if (request.Food is null)
+            {
+                return default;
+            }
+
+            var newFood = await _repository.UpdateAsync(entity: request.Food, cancellationToken);
+
+            return newFood;
+        }
+    }
+}
