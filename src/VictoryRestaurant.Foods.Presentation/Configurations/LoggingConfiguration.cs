7431ac9c7b28323b2c6b4d39d25636c87e5d4cae
DimaInNature <dimainnature@yandex.ru>
2023-02-09 04:32:05 +0300

    feat: Add Serilog.

---
+++ src/VictoryRestaurant.Foods.Presentation/Configurations/LoggingConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace VictoryRestaurant.Foods.Presentation.Configurations;
+
+internal static class LoggingConfiguration
+{
+    public static void AddLoggingConfiguration(
+        this IServiceCollection services,
+        ConfigureHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog(
+            configureLogger: (context, services, configuration) =>
+                configuration.ReadFrom.Configuration(configuration: context.Configuration)
+                .ReadFrom.Services(services));
+
+        services.AddHttpContextAccessor();
+    }
+}
