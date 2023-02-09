91fb26991a51f0b379bbb4dfd87b72165c97c799
DimaInNature <dimainnature@yandex.ru>
2023-02-09 04:10:11 +0300

    refactoring: Remove Persistence tier.

--- src/VictoryRestaurant.Foods.Persistence/global.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.ValueGeneration;
-global using System.Linq.Expressions;
-global using VictoryRestaurant.Foods.Domain.Contracts.Repositories;
-global using VictoryRestaurant.Foods.Domain.Entities;
-global using VictoryRestaurant.Foods.Persistence.Context;
-global using VictoryRestaurant.Foods.Persistence.Context.Configurations;
