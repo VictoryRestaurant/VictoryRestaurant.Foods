0cd6ff29fa913c56f711b0502424dcc900e0e4c6
DimaInNature <dimainnature@yandex.ru>
2023-02-09 04:11:31 +0300

    feat: Create MediatR profiles.

---
+++ src/VictoryRestaurant.Foods.Infrastructure/DependencyInjection/MediatR/Profiles/FoodMediatRProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace VictoryRestaurant.Foods.Infrastructure.DependencyInjection.MediatR.Profiles;
+
+internal static class FoodMediatRProfile
+{
+    public static void AddFoodMediatRProfile(this IServiceCollection services)
+    {
+        // Commands
+        services.AddScoped<IRequest<FoodEntity?>, CreateFoodCommand>();
+        services.AddScoped<IRequestHandler<CreateFoodCommand, FoodEntity?>, CreateFoodCommand.Handler>();
+
+        services.AddScoped<IRequest<Unit>, DeleteFoodCommand>();
+        services.AddScoped<IRequestHandler<DeleteFoodCommand, Unit>, DeleteFoodCommand.Handler>();
+
+        services.AddScoped<IRequest<FoodEntity?>, UpdateFoodCommand>();
+        services.AddScoped<IRequestHandler<UpdateFoodCommand, FoodEntity?>, UpdateFoodCommand.Handler>();
+
+        // Queries
+        services.AddScoped<IRequest<IEnumerable<FoodEntity>>, GetAllFoodListQuery>();
+        services.AddScoped<IRequestHandler<GetAllFoodListQuery, IEnumerable<FoodEntity>>, GetAllFoodListQuery.Handler>();
+
+        services.AddScoped<IRequest<FoodEntity?>, GetFoodByConditionQuery>();
+        services.AddScoped<IRequestHandler<GetFoodByConditionQuery, FoodEntity?>, GetFoodByConditionQuery.Handler>();
+
+        services.AddScoped<IRequest<IEnumerable<FoodEntity>>, GetFoodListByConditionQuery>();
+        services.AddScoped<IRequestHandler<GetFoodListByConditionQuery, IEnumerable<FoodEntity>>, GetFoodListByConditionQuery.Handler>();
+    }
+}
---
+++ src/VictoryRestaurant.Foods.Infrastructure/DependencyInjection/MediatR/Profiles/FoodTypeMediatRProfile.cs
@@ -0,0 +1,27 @@
+﻿namespace VictoryRestaurant.Foods.Infrastructure.DependencyInjection.MediatR.Profiles;
+
+internal static class FoodTypeMediatRProfile
+{
+    public static void AddFoodTypeMediatRProfile(this IServiceCollection services)
+    {
+        // Commands
+        services.AddScoped<IRequest<FoodTypeEntity?>, CreateFoodTypeCommand>();
+        services.AddScoped<IRequestHandler<CreateFoodTypeCommand, FoodTypeEntity?>, CreateFoodTypeCommand.Handler>();
+
+        services.AddScoped<IRequest<Unit>, DeleteFoodTypeCommand>();
+        services.AddScoped<IRequestHandler<DeleteFoodTypeCommand, Unit>, DeleteFoodTypeCommand.Handler>();
+
+        services.AddScoped<IRequest<FoodEntity?>, UpdateFoodTypeCommand>();
+        services.AddScoped<IRequestHandler<UpdateFoodTypeCommand, FoodEntity?>, UpdateFoodTypeCommand.Handler>();
+
+        // Queries
+        services.AddScoped<IRequest<IEnumerable<FoodTypeEntity>>, GetAllFoodTypeListQuery>();
+        services.AddScoped<IRequestHandler<GetAllFoodListQuery, IEnumerable<FoodEntity>>, GetAllFoodListQuery.Handler>();
+
+        services.AddScoped<IRequest<FoodTypeEntity?>, GetFoodTypeByConditionQuery>();
+        services.AddScoped<IRequestHandler<GetFoodTypeByConditionQuery, FoodTypeEntity?>, GetFoodTypeByConditionQuery.Handler>();
+
+        services.AddScoped<IRequest<IEnumerable<FoodTypeEntity>>, GetFoodTypeListByConditionQuery>();
+        services.AddScoped<IRequestHandler<GetFoodTypeListByConditionQuery, IEnumerable<FoodTypeEntity>>, GetFoodTypeListByConditionQuery.Handler>();
+    }
+}
