9744502b67fc807df7e18549bb2ad946a4288de8
DimaInNature <dimainnature@yandex.ru>
2023-02-09 04:11:49 +0300

    feat: Create MediatRConfiguration.

---
+++ src/VictoryRestaurant.Foods.Infrastructure/DependencyInjection/MediatR/MediatRConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace VictoryRestaurant.Foods.Infrastructure.DependencyInjection.MediatR;
+
+public static class MediatRConfiguration
+{
+    public static void AddMediatRConfiguration(this IServiceCollection services)
+    {
+        services.AddMediatR(assemblies: Assembly.GetExecutingAssembly());
+
+        services.AddFoodMediatRProfile();
+
+        services.AddFoodTypeMediatRProfile();
+    }
+}
