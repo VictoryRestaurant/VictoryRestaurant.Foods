195cfbc6341a44c8f13a60fb76124d555b40c9e6
DimaInNature <dimainnature@yandex.ru>
2023-02-11 06:56:00 +0300

    feat: Create empty FoodTypesController.

---
+++ src/VictoryRestaurant.Foods.Presentation/Controllers/FoodTypesController.cs
@@ -0,0 +1,9 @@
+﻿namespace VictoryRestaurant.Foods.Presentation.Controllers;
+
+[Route(template: "api/foodtypes")]
+[Produces(contentType: "application/json")]
+[ApiController]
+public sealed class FoodTypesController : ControllerBase
+{
+
+}
