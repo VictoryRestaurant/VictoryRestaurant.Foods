68f2613cedfc949229a05bbbdabf998bb2b549b1
DimaInNature <dimainnature@yandex.ru>
2023-02-11 06:45:34 +0300

    unit: Create test for GetFoodTypeByConditionQuery.

---
+++ src/VictoryRestaurant.Foods.Tests/UnitTests/Queries/FoodTypes/GetFoodTypeByConditionQueryTests.cs
@@ -0,0 +1,32 @@
+﻿namespace VictoryRestaurant.Foods.Tests.UnitTests.Queries.FoodTypes;
+
+public sealed class GetFoodTypeByConditionQueryTests
+{
+    [Fact]
+    public async ValueTask Handle()
+    {
+        // Arrange
+        await using var context = DbContextFactory.BuildApplicationContext();
+
+        var repository = new FoodTypeEntityRepository(context);
+
+        var handler = new GetFoodTypeByConditionQuery.Handler(repository);
+
+        var generatedFoodType = FoodTypeEntityGenerator.Generate();
+
+        await context.FoodTypes.AddAsync(entity: generatedFoodType);
+
+        await context.SaveChangesAsync();
+
+        Expression<Func<FoodTypeEntity, bool>> condition =
+            foodType => foodType.Equals(generatedFoodType);
+
+        var query = new GetFoodTypeByConditionQuery(predicate: condition);
+
+        // Act
+        var foodType = await handler.Handle(request: query, cancellationToken: default);
+
+        // Assert
+        foodType.Should().BeEquivalentTo(expectation: generatedFoodType);
+    }
+}
