a4a9bc4cbdf7402d974812373a829beacd9ea42f
DimaInNature <dimainnature@yandex.ru>
2023-03-15 19:44:30 +0300

    feat: Add CreateFoodCommand validation.

---
+++ src/VictoryRestaurant.Foods.Infrastructure/Validation/ValidationDependencyInjectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace VictoryRestaurant.Foods.Infrastructure.Validation;
+
+public static class ValidationDependencyInjectionConfiguration
+{
+    public static IServiceCollection AddValidation(this IServiceCollection services)
+    {
+        services.AddFluentValidationAutoValidation();
+
+        services.AddValidatorsFromAssemblyContaining<CreateFoodRequestValidator>();
+
+        return services;
+    }
+}
